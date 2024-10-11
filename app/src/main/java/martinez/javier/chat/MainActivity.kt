8ce6d118135cd77a9b34bd939bb348bc106d0ca5
Javier Martinez <eduar_etza@hotmail.com>
2024-10-11 09:59:03 -0600

    Create NBView

--- app/src/main/java/martinez/javier/chat/MainActivity.kt
+++ app/src/main/java/martinez/javier/chat/MainActivity.kt
@@ -5,16 +5,87 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import martinez.javier.chat.Fragmentos.FragmentChats
+import martinez.javier.chat.Fragmentos.FragmentPerfil
+import martinez.javier.chat.Fragmentos.FragmentUsuarios
+import martinez.javier.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var biding : ActivityMainBinding
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        biding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+
+        //Fragmento por defecto
+        verFragmentoPerfil()
+
+        biding.bottomNV.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_perfil->{
+                    //Visualizar el fragmento Perfil
+                    verFragmentoPerfil()
+                    true
+                }
+                R.id.item_usuarios->{
+                    //Visualizar el fragmento Usuarios
+                    verFragmentosUsuarios()
+                    true
+                }
+                R.id.item_chats->{
+                    //Visualizar el fragmento Chats
+                    verFragmentosChats()
+                    true
+                }
+                else->{
+                    false
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun verFragmentoPerfil(){
+        biding.tvTitulo.text = "Perfil"
+
+        val fragment = FragmentPerfil()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(biding.fragmentoFL.id, fragment, "Fragment Perfil")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentosUsuarios(){
+        biding.tvTitulo.text = "Usuarios"
+
+        val fragment = FragmentUsuarios()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(biding.fragmentoFL.id, fragment, "Fragment Usuarios")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentosChats(){
+        biding.tvTitulo.text = "Chats"
+
+        val fragment = FragmentChats()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(biding.fragmentoFL.id, fragment, "Fragment Chats")
+        fragmentTransaction.commit()
+    }
+
+
 }
