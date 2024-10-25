dbffd2ccab81602e4bdcab8d1f411a6aadee6bee
Javier Martinez <eduar_etza@hotmail.com>
2024-10-25 15:02:42 -0600

    edit image BD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,10 @@ dependencies {
     implementation(libs.circle.image)
     implementation(libs.glide)
 
+    implementation(libs.firebase.storage)
+
+
+
    //SE NECESITAN SI SE USA LA FORMA ANTIGUA EN PLUGINS A NIVEL MODULE (ARRIBA) Y PLUGINS A NIVEL PROJECT
     //implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     //implementation("com.google.firebase:firebase-analytics")
--- app/src/main/java/martinez/javier/chat/EditarInformacionActivity.kt
+++ app/src/main/java/martinez/javier/chat/EditarInformacionActivity.kt
@@ -1,9 +1,14 @@
 package martinez.javier.chat
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +18,9 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.FirebaseStorageKtxRegistrar
+import kotlinx.coroutines.selects.whileSelect
 import martinez.javier.chat.databinding.ActivityEditarInformacionBinding
 
 class EditarInformacionActivity : AppCompatActivity() {
@@ -20,6 +28,9 @@ class EditarInformacionActivity : AppCompatActivity() {
     private lateinit var binding : ActivityEditarInformacionBinding //Acceso a los datos de la interfaz
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var progressDialog : ProgressDialog
+
+    //Almacenar imagen selecionada de la galeria
+    private var imagUri : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +54,16 @@ class EditarInformacionActivity : AppCompatActivity() {
         binding.IbRegresar.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        //Evento para boton de imagen de perfil
+        binding.IvEditarImg.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){//Version tiramisu o superior no se necesita obligatoriamente el perfimiso de almacenamiento
+                abrirGaleria()
+            }else{//Obligatori perdir permiso (Tipico mensaje de aceptar permisos)
+                solicitarPermisoAlmacenamiento.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+
         //Evento para boton actualizar
         binding.btnActualizar.setOnClickListener {
             validarInformacion()
@@ -54,6 +75,101 @@ class EditarInformacionActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private fun abrirGaleria() {
+        //Iniciar el selector de imagen
+        val intent = Intent(Intent.ACTION_PICK)
+        //VIsualizar solo archivos tipo imagen
+        intent.type = "image/*"//Al abrir los archibos de la galeria solo mostrara imagenes
+        galeriaARL.launch(intent)
+    }
+
+    //Para obtener la imagen selecionada de la galeria
+    private var galeriaARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){resultado->
+            if (resultado.resultCode == Activity.RESULT_OK){//Comporbar si la imagen fue seleccionada
+                val data = resultado.data
+                imagUri = data!!.data
+                subirImagenStorage(imagUri)
+            }else{
+                Toast.makeText(
+                    this,
+                    "Cancelado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private fun subirImagenStorage(imagUri: Uri?) {
+        progressDialog.setMessage("Subiendo imagen a Storage")
+        progressDialog.show()
+
+        //Nombre y ruta de la carpeta donde se va a guardar esa imagen
+        val rutaimagen = "imagenesPerfil/" + firebaseAuth.uid //Nombre de la carpeta y nombre de la imagen en si sera el uid del usuario
+        val ref = FirebaseStorage.getInstance().getReference(rutaimagen)
+        ref.putFile(imagUri!!)
+            .addOnSuccessListener { taskSnapshot->
+                val uriTask = taskSnapshot.storage.downloadUrl
+                while (!uriTask.isSuccessful);
+                val urlImagenCargada = uriTask.result.toString()
+                if (uriTask.isSuccessful){
+                    actualizarInfoBD(urlImagenCargada)
+                }
+
+            }
+            .addOnFailureListener { e->
+
+            }
+    }
+
+    private fun actualizarInfoBD(urlImagenCargada: String) {
+        progressDialog.setMessage("Actualizando imagen")
+        progressDialog.show()
+
+        val hashMap : HashMap<String, Any> = HashMap()
+        if (imagUri!=null){
+            hashMap["imagen"] = urlImagenCargada//nombre del atributo de la base de datos "imagen"
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")//Nombre de la base de datos"
+        ref.child(firebaseAuth.uid!!)//acceder al usuario actual que realiza la actualizacion
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Se actualizó la imagen correctamente",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    e.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+
+            }
+
+    }
+
+    //Solicitud del permiso de almacenamiento
+    private val solicitarPermisoAlmacenamiento =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){esConcedido->
+            //Si el permiso es concedido
+            if (esConcedido){
+                abrirGaleria()
+            }else{
+                Toast.makeText(
+                    this,
+                    "Permiso de almacenamiento denegado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
 
     private var nombres = ""
     private fun validarInformacion() {
