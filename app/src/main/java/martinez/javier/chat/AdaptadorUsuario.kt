add8472f3dba2d6d07a5f24e64bcd7a38a6ebad2
Javier Martinez <eduar_etza@hotmail.com>
2024-10-30 13:40:28 -0600

    User list and basic search

---
+++ app/src/main/java/martinez/javier/chat/AdaptadorUsuario.kt
@@ -0,0 +1,61 @@
+package martinez.javier.chat
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class AdaptadorUsuario(//Implementar miembros
+    //Parametros
+    context : Context,
+    //Modelo Usuario
+    listaUsuarios : List<Usuario>) : RecyclerView.Adapter<AdaptadorUsuario.ViewHolder?>() {
+
+        private val context : Context
+        private val listaUsuarios : List<Usuario>//Modelo Usuario
+
+        init {
+            this.context = context
+            this.listaUsuarios = listaUsuarios
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Conexion del adaptador con el diseño item_usuario
+        val view : View = LayoutInflater.from(context).inflate(R.layout.item_usuario,parent,false)//Ruta donde se encuentra el diseño
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        //Obtener el tamaño total de la lista de usuarios
+        return listaUsuarios.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Obtener y mostrar informacion dentro de las vistas (dentro de los texview y el imageview)
+        val usuario : Usuario = listaUsuarios[position]
+        holder.uid.text = usuario.uid
+        holder.email.text = usuario.email
+        holder.nombres.text = usuario.nombres
+        Glide.with(context).load(usuario.imagen).placeholder(R.drawable.ic_imagen_perfil).into(holder.imagen)
+    }
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        //Declarar vistas del diseño item_usuario para posteriormente inicializarlas
+        var uid : TextView
+        var email : TextView
+        var nombres : TextView
+        var imagen : ImageView
+
+        init {
+            uid = itemView.findViewById(R.id.item_uid)
+            email = itemView.findViewById(R.id.item_email)
+            nombres = itemView.findViewById(R.id.item_nombre)
+            imagen = itemView.findViewById(R.id.item_imagen)
+        }
+    }
+}
--- app/src/main/java/martinez/javier/chat/Fragmentos/FragmentUsuarios.kt
+++ app/src/main/java/martinez/javier/chat/Fragmentos/FragmentUsuarios.kt
@@ -1,22 +1,129 @@
 package martinez.javier.chat.Fragmentos
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import martinez.javier.chat.AdaptadorUsuario
 import martinez.javier.chat.R
+import martinez.javier.chat.Usuario
+import martinez.javier.chat.databinding.FragmentUsuariosBinding
 
 
 class FragmentUsuarios : Fragment() {
 
+    private lateinit var binding : FragmentUsuariosBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuarios, container, false)
+    private lateinit var mContext : Context
+    private var usuarioAdaptador : AdaptadorUsuario?=null
+    private var usuarioLista : List<Usuario>?=null
+
+    //Inicializar contexto
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+       //Inicializacion del binding
+        binding = FragmentUsuariosBinding.inflate(layoutInflater, container, false)
+
+        binding.RVUsuarios.setHasFixedSize(true)
+        binding.RVUsuarios.layoutManager = LinearLayoutManager(mContext)
+
+        //Inicializar lista de usuarios
+        usuarioLista = ArrayList()
+
+        binding.etBuscarUsuario.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(usuario: CharSequence?, p1: Int, p2: Int, p3: Int) {//p0 por usuario
+                buscarUsuarios(usuario.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })//object: implementar miembros
+
+        listarUsuarios()
+
+        return binding.root
+    }
+
+    private fun listarUsuarios() {
+        //Obtener uid del usuario actual
+        val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
+        //Referencia a la base de datos
+        val reference = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("nombres")//Nombre de la base de datos y listarlos con respecto a su nombre
+        reference.addValueEventListener(object : ValueEventListener{//object: implementar miembros
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //Primero limpiar la lista antes de llenarla
+                (usuarioLista as ArrayList<Usuario>).clear() //Modelo: Usuario
+                //Si el campo de busqueda esta vacio lista todos los usuarios
+                //O sea cuando deje de escribir se listaran todos los usuarios
+                if (binding.etBuscarUsuario.text.toString().isEmpty()){
+                    //Recorrido a la base de datos
+                    for (sn in snapshot.children){
+                        val usuario : Usuario? = sn.getValue(Usuario::class.java)//Obtener informacion dentro del modelo
+                        //condicon para listar a todos los usuario, excepto a nosotros
+                        if (!(usuario!!.uid).equals(firebaseUser)){ //Si el uid recuperado no es igual al uid del usuario actual es ahi cuando se va a llenar la lista
+                            (usuarioLista as ArrayList<Usuario>).add(usuario)
+                        }
+                    }
+                }
+                usuarioAdaptador = AdaptadorUsuario(mContext, usuarioLista!!)
+                binding.RVUsuarios.adapter = usuarioAdaptador
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+    }
+
+    //SOLUCIONAR ¿?
+    //Busqueda de inicio a fin, o sea buscar un usuario en especifico, usando la secuencia de primer nombre...
+    private fun buscarUsuarios (usuario : String){
+        //Obtener uid del usuario actual
+        val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
+        //Referencia a la base de datos
+        val reference = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("nombres")//Nombre de la base de datos y bbuscarlos con respecto a su nombre
+            .startAt(usuario).endAt(usuario+"\uf8ff")
+        reference.addValueEventListener(object : ValueEventListener{//object: implementar miembros
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //Limpiar la lista
+                (usuarioLista as ArrayList<Usuario>).clear()
+                //Recorrido a la base de datos
+                for (ss in snapshot.children){
+                    val usuario : Usuario ?= ss.getValue(Usuario::class.java)
+                    //condicon para listar a todos los usuario, excepto a nosotros
+                    if (!(usuario!!.uid).equals(firebaseUser)){
+                        (usuarioLista as ArrayList<Usuario>).add(usuario)
+                    }
+                }
+                usuarioAdaptador = AdaptadorUsuario(context!! , usuarioLista!!)
+                binding.RVUsuarios.adapter = usuarioAdaptador
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
 }
